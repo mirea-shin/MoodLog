98e6b98748a20ddb5bd8d7246ce134b435af94ca
Leya <m.shin6764@gmail.com>
2025-09-06 15:00:30 -0400

    Add layout component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from 'react-router-dom';
 import AuthPage from './pages/auth/AuthPage';
+import MoodLogPage from './pages/moodLog/MoodLogPage';
+
+import Layout from './shared/components/ui/Layout';
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<AuthPage />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<AuthPage />} />
+        <Route path="/moodLog" element={<MoodLogPage />} />
+      </Route>
     </Routes>
   );
 }
